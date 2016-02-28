51f105e9bef18cb31c6ee1e8e052fb13823705e0
Auzarius <auzarius@gmail.com>
2016-02-28 13:21:01 -0500

    Convert gulpfile to es2015

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,30 +1,21 @@
-var gulp 		= require("gulp"),
-	babel 		= require("gulp-babel"),
-	concat  	= require("gulp-concat"),
-	//connect		= require("gulp-connect"),
-	cssnano		= require("gulp-cssnano"),
-	jshint		= require("gulp-jshint"),
-	ngAnnotate  = require("gulp-ng-annotate"),
-	plumber		= require("gulp-plumber"),
-	rename		= require("gulp-rename"),
-	sass		= require("gulp-ruby-sass"),
-	sourcemaps  = require("gulp-sourcemaps"),
- 	uglify 		= require("gulp-uglify"),
-	watch		= require("gulp-watch");
-	
-var livePort	= 1330;
+"use strict";
 
-/*
-gulp.task('connect', function() {
-	connect.server({
-		root: './public/views/',
-		port: 1330,
-		livereload: true
-	});
-});
-*/
+import gulp from "gulp";
+import babel from "gulp-babel";
+import concat from "gulp-concat";
+import cssnano from "gulp-cssnano";
+import jshint from "gulp-jshint";
+import ngAnnotate from "gulp-ng-annotate";
+import plumber from "gulp-plumber";
+import rename from "gulp-rename";
+import sass from "gulp-ruby-sass";
+import sourcemaps from "gulp-sourcemaps";
+import uglify from "gulp-uglify";
+import watch from "gulp-watch";
 
-gulp.task('annotate', function() {
+let livePort	= 1330;
+
+gulp.task('annotate', () => {
 	return gulp.src('./ng-app/**/*.js')
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
@@ -35,38 +26,34 @@ gulp.task('annotate', function() {
 		.pipe(rename({ extname: ".min.js" }))
 		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest('./public/dist/js/'));
-		//.pipe(connect.reload());
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
 	return sass(['./public/dev/css/**/*.scss', './public/dev/css/**/*.sass'])
 		.pipe(plumber())
 		.pipe(gulp.dest('./public/dev/css'));
-		//.pipe(connect.reload());
 });
 
-gulp.task('mini-css', function() {
+gulp.task('mini-css', () => {
 	return gulp.src('./public/dev/css/**/*.css')
 		.pipe(plumber())
 		.pipe(cssnano())
 		.pipe(concat('all.min.css'))
 		.pipe(gulp.dest('./public/dist/css'));
-		//.pipe(connect.reload());
 });
 
-gulp.task("babel", function () {
-	return gulp.src(["**/*.es6", '!./{node_modules,node_modules/**}'])
+gulp.task("babel", () => {
+	return gulp.src(["**/*.babel.js", '!./{node_modules,node_modules/**}'])
 		.pipe(plumber())
 		.pipe(babel())
 		.pipe(jshint())
 		.pipe(gulp.dest("./public/dist/js/"))
 		.pipe(rename({ extname: ".min.js" }))
 		.pipe(gulp.dest("./public/dist/js/"));
-		//.pipe(connect.reload());
 });
 
-gulp.task("watch", function() {
-	console.log("Gulp-Watch has begun watching ES6, Angular, CSS and SASS files.\n");
+gulp.task("watch", () => {
+	console.log("Gulp-Watch has begun watching ES6, Angular, CSS and SASS files.");
 	gulp.watch('./ng-app/**/*.js', ['annotate']);
 	gulp.watch(['./public/dev/css/**/*.scss', './public/dev/css/**/*.sass'], ['sass']);
 	gulp.watch('./public/dev/css/**/*.css', ['mini-css']);
